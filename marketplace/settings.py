a0e4deea01a4150be6170bb32c04d793da38669d
Maulik Joshi <jmaulik21@gmail.com>
2026-08-12 22:40:43 +0545

    Configure project for Render deployment: add requirements.txt, build.sh, WhiteNoise for static files, Gunicorn WSGI, CSRF trusted origins, and dynamic settings

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -12,19 +12,44 @@ if env_file.exists():
             os.environ.setdefault(k.strip(), v.strip())
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&!7=4y@z$eymqm#rhw6h1&%6=x*=q#=%49c$04j_w^=2iokbh8')
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+if '*' not in ALLOWED_HOSTS:
+    ALLOWED_HOSTS.extend(['.onrender.com', 'localhost', '127.0.0.1'])
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com',
+    'http://127.0.0.1',
+    'http://localhost',
+]
+rendered_url = os.environ.get('RENDER_EXTERNAL_URL')
+if rendered_url:
+    CSRF_TRUSTED_ORIGINS.append(rendered_url)
 
 INSTALLED_APPS = [
-    'jazzmin', 'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
-    'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles',
-    'products.apps.ProductsConfig', 'blog.apps.BlogConfig', 'pages.apps.PagesConfig', 'sitesetting.apps.SitesettingConfig',
+    'jazzmin',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
+    'products.apps.ProductsConfig',
+    'blog.apps.BlogConfig',
+    'pages.apps.PagesConfig',
+    'sitesetting.apps.SitesettingConfig',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware', 'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -35,17 +60,34 @@ TEMPLATES = [{
     'DIRS': [BASE_DIR / 'Templates'],
     'APP_DIRS': True,
     'OPTIONS': {'context_processors': [
-        'django.template.context_processors.request', 'django.contrib.auth.context_processors.auth',
-        'django.contrib.messages.context_processors.messages', 'pages.context_processors.page_links',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+        'pages.context_processors.page_links',
         'sitesetting.context_processors.site_settings',
     ]},
 }]
 
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}}
+try:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+            conn_max_age=600
+        )
+    }
+except ImportError:
+    DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}}
+
 AUTH_PASSWORD_VALIDATORS = [{'NAME': f'django.contrib.auth.password_validation.{v}'} for v in ['UserAttributeSimilarityValidator', 'MinimumLengthValidator', 'CommonPasswordValidator', 'NumericPasswordValidator']]
 
 LANGUAGE_CODE, TIME_ZONE, USE_I18N, USE_TZ = 'en-us', 'UTC', True, True
-STATIC_URL, STATICFILES_DIRS = 'static/', [BASE_DIR / "marketplace" / "static"]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "marketplace" / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL, MEDIA_ROOT = '/media/', BASE_DIR / 'media'
 
 EMAIL_MICROSERVICE_URL = os.environ.get('EMAIL_MICROSERVICE_URL', 'https://api.resend.com/emails')
